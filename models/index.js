151ba5f362f614d7c0f7de710f707a3faa8e95c2
“stevengo497” <ngo.steve497@yahoo.com>
2018-04-20 12:05:48 -0600

    adding to heroku

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely");
 
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect("mongodb://localhost/szechuan");
+}
+
 module.exports.Album = require("./album.js");
